7eafd3b2077998981af3b7d1734c0d240221c864
abdul rahman alkhatib <mrabdo16th@hotmail.com>
2015-10-15 02:35:45 +0800

    about done

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,52 @@
-var cards = $('.card');
+var cards = $('.flipable-card');
 
+//$("#card").flip();
+//$("#card-1").flip();
 
 
 for (i = 0; i<cards.length; i++) {
 
 	card = cards[i];
 
-	cardInfo = card.children[0];
-	cardInfo.hidden = true;
+	$(card).flip();
 
-	card.addEventListener("click", function() {
 
-		if (this.show == false) {
 
-			this.show = true;
-			
-		}
-		else{
 
-			this.show = false;
-		}
+}
 
-		var info = this.children[0];
-		info.hidden = this.show;
+var products_name=[];
+var product_images=[];
+var product_description=[];
+
+$('.bxSlider').bxSlider({
+  auto: true
+});
+
+bxControls = $('.bx-controls');
+$(bxControls[0]).hide();
+
+products = $(".products");
+modalLabel = $("#modal-label");
+modalImage = $("#modal-image");
+modalDescription = $("#modal-text");
+
+for (i = 0; i<products.length; i++) {
+
+	product = $(products[i]);
+	
+	product.click(function() {
+
+		label = $(this).data("label");
+		description = $(this).data("description");
+		image = $(this).data("image");
+		
+		modalLabel.html(label);
+		modalDescription.html(description);
+		modalImage.attr("src", image);
+
 	});
 
-
+	
 
 }
