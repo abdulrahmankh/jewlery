cee25899b455c2b1e5d774dbe6ef48829ab40dc1
abdul rahman alkhatib <mrabdo16th@hotmail.com>
2015-10-15 03:09:27 +0800

    adding commints and butifing

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,38 @@
+// Copyright (c) 2015, 2016 All Right Reserved
+//
+// <author>Abdul Raham Alkhatib</author>
+// <email>mrabdo16th@hotmail.com</email>
+// <date>2015-10-15</date>
+// <summary>This JS file controls the website "Miss Jewlery Passion" 
+// adding flip functionality and calling a modal on each product</summary>
+
+
+// giving all card a flip property using (flip.js)
 var cards = $('.flipable-card');
 
-//$("#card").flip();
-//$("#card-1").flip();
-
-
 for (i = 0; i<cards.length; i++) {
-
 	card = cards[i];
-
 	$(card).flip();
-
-
-
-
 }
 
-var products_name=[];
-var product_images=[];
-var product_description=[];
+//------------------------------------//
 
+// createing the slid
 $('.bxSlider').bxSlider({
   auto: true
 });
-
+	// fixing a small bug where the number of teh image does not look good
 bxControls = $('.bx-controls');
 $(bxControls[0]).hide();
 
+//--------------------------------//
+
+
+// taking care of the modal data when a product is clicked form more info
 products = $(".products");
 modalLabel = $("#modal-label");
 modalImage = $("#modal-image");
-modalDescription = $("#modal-text");
+modalDescription = $("#modal-description");
 
 for (i = 0; i<products.length; i++) {
 
@@ -40,13 +43,12 @@ for (i = 0; i<products.length; i++) {
 		label = $(this).data("label");
 		description = $(this).data("description");
 		image = $(this).data("image");
-		
+
 		modalLabel.html(label);
 		modalDescription.html(description);
 		modalImage.attr("src", image);
 
 	});
-
-	
-
 }
+
+//----------------------//
